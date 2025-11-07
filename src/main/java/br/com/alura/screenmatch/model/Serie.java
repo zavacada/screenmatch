99af4a4bcd8aadc7fb56e6fdea07ab219c406b87
zavacada <jcfcardoso5555@gmail.com>
2025-11-07 10:51:46 -0300

    feat: salvando dados no banco

--- src/main/java/br/com/alura/screenmatch/model/Serie.java
+++ src/main/java/br/com/alura/screenmatch/model/Serie.java
@@ -2,6 +2,8 @@ package br.com.alura.screenmatch.model;
 
 import jakarta.persistence.*;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.OptionalDouble;
 
 @Entity
@@ -23,6 +25,11 @@ public class Serie {
     private String poster;
     private String sinopse;
 
+    @Transient
+    private List<Episodio> episodios = new ArrayList<>();
+
+
+
     public Serie(DadosSerie dadosSerie) {
         this.titulo = dadosSerie.titulo();
         this.totalTemporadas = dadosSerie.totalTemporadas();
@@ -31,6 +38,26 @@ public class Serie {
         this.atores = dadosSerie.atores();
         this.poster = dadosSerie.poster();
         this.sinopse = dadosSerie.sinopse();
+
+
+    }
+
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public List<Episodio> getEpisodios() {
+        return episodios;
+    }
+
+    public void setEpisodios(List<Episodio> episodios) {
+        this.episodios = episodios;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getTitulo() {
